28396473a8fefe0e6ca7e41c23ae0751cc3c4b7d
pomme <apple.cjcfardel@yahoo.com>
2014-12-16 16:09:16 +0000

    create a data object to brige map and gdp tables

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var gdp = getCountryGDP(),
     codes= getCountryCodes(),
-    tBody = document.getElementById("gdpTableBody");  
+    codeNumToA3 = {},
+    data = {};
+
+/* data */
+
+/* events */
 
 /* gdb map */
 var svg = d3.select("svg#gdpMap"),
@@ -11,73 +16,95 @@ var svg = d3.select("svg#gdpMap"),
 
 d3.json(json, function(err, map) {
   if (err) { return console.log(error); }
-    var codeNumToA3 = {};
-    svg.selectAll("path")
-       .data(topojson.feature(map, map.objects.countries).features)
-       .enter()
-       .append("path")
-       .attr("id", function(d) {
-         var a3Code = "NAC"; // NAC = not a code
-         codes.forEach(function(code) {
-           if (d.id === parseInt(code.num)) {
-             codeNumToA3[d.id] = a3Code = code.a3;
-             return;
-           }
-         });
-         return a3Code;
-       })
-       .attr("d", path)
-       .style("fill", function(d) {
-         //var value = d.id;
-         //if (value) {return color(value);}
-         //else {return "lightgrey";}
+  /* draw path */  
+  svg.selectAll("path")
+     .data(topojson.feature(map, map.objects.countries).features)
+     .enter()
+     .append("path")
+     .attr("id", function(d) {
+       var a3Code = "NAC"; // NAC = not a code
+
+       /* data */
+       // create a country code mapping: id (number) to a3 code 
+       codes.forEach(function(code) {
+         if (d.id === parseInt(code.num)) {
+           codeNumToA3[d.id] = a3Code = code.a3;
+           return;
+         }
        });
-    
-    svg.selectAll("text")
-       .data(topojson.feature(map, map.objects.countries).features)
-       .enter()
-       .append("text")
-       .attr("transform", function(d) {
-         return "translate(" + path.centroid(d) + ")"; 
-       })
-       .attr("class", function(d) { 
-         var country = {},
-             pathNode;
-         /* events */
-         this.addEventListener("mouseout", function(){
-           if (pathNode !== null) {
-             pathNode.classList.remove("fill-black");
-           }
-         }, false);
-         this.addEventListener("mouseover", function() {
-           //var pathNode;
-           country.id = d.id;
-           country.a3 = codeNumToA3[d.id];
-           gdp.forEach(function(gdp) {
-           if (gdp.countryCode === codeNumToA3[d.id]) {
-             country.name = gdp.countryName;
-             country.gdp = gdp.millionUSD;
-             //console.log("map: " + d.properties.name + "[" + d.id + "]"); 
-             //console.log("gdp: " + gdp.countryName + "[" + gdp.countryCode + "], " + gdp.millionUSD);
-           }
-           }); 
-           pathNode = document.getElementById(country.a3);
-           if (pathNode !== null) {
-             console.log(country);
-             pathNode.classList.add("fill-black"); 
-           } else {
-             console.log(d.properties.name+"["+d.id+"]:" + "no num to a3 mapping!");
-           }
-         });
-         return "pin code" + codeNumToA3[d.id]; 
-       })
-       .attr("dy", "1em")
-       .text(function(d) {
-         return d.properties.name;
+       // create a data table: id to gdp data
+       gdp.forEach(function(gdp) {
+         if (gdp.countryCode === codeNumToA3[d.id]) {
+           data[d.id] = {"a3"  : codeNumToA3[d.id]};
+           data[d.id] = {"num" : gdp.countryCode};
+           data[d.id] = {"name": gdp.countryName};
+           data[d.id] = {"gdp" : gdp.millionUSD};
+           return;
+         }
        });
+       delete codes, gdp;       
+       
+       return "p" + a3Code;
+     })
+     .attr("d", path)
+     .style("fill", function(d) {
+       //var value = d.id;
+       //if (value) {return color(value);}
+       //else {return "lightgrey";}
+     });
+
+  /* draw text */
+  svg.selectAll("text")
+     .data(topojson.feature(map, map.objects.countries).features)
+     .enter()
+     .append("text")
+     .attr("transform", function(d) {
+       return "translate(" + path.centroid(d) + ")"; 
+     })
+     .attr("class", function(d) {
+       /* events */
+       onPinEvent(this, d, "mouseover", "mouseout");
+       onPinEvent(this, d, "click");
+       return "pin"; 
+     })
+     .attr("data-code", function(d) {return codeNumToA3[d.id];})
+     .attr("dy", "1em")
+     .text(function(d) {
+       var marker = d.properties.name;
+       // use country names base on the gdp table
+       if (data[d.id] && data[d.id].name) { marker = data[d.id].name; } 
+       //else { console.log(marker); }
+       return marker;
+     });
+  //console.log(data); 
 });
 
+var onPinEvent = function(that, d, eventType1, eventType2) {
+  var pathNode,
+      eventType;
+          
+  that.addEventListener(eventType1, function() {
+    /* highlight the pin with its land */ 
+    pathNode = document.getElementById("p" + codeNumToA3[d.id]);
+    if (pathNode !== null) {
+      pathNode.classList.add("fill-black"); 
+    } else {
+      //console.log(d.properties.name+"["+d.id+"]:" + "no num to a3 mapping!");
+    }
+    /* display data */
+    if (data[d.id]) { console.log(data[d.id].gdp); }    
+  }, false);
+
+  that.addEventListener(eventType2, function(e){
+    pathNode = document.getElementById("p"+e.target.dataset.code);
+    if (pathNode !== null) {
+      pathNode.classList.remove("fill-black");
+    }
+  }, false);
+}
+
 /* gdp table */
+var tBody = document.getElementById("gdpTableBody");  
 gdp.forEach(function(c) {
   var tr, td;
   tr = document.createElement("tr");
